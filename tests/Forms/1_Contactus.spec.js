0e2f8738ff7f40568989f174a4ec24eed74b83fe
surya <surya.y@pattemdigital.com>
2025-12-15 15:48:19 +0530

    add waitforload

--- tests/Forms/1_Contactus.spec.js
+++ tests/Forms/1_Contactus.spec.js
@@ -3,37 +3,34 @@ import path from 'path';
 
 // selected the job seeker from dropdown and upload the resume and submit the form  //
 test('Fill PattemDigital Contact Form job seeker', async ({ page }) => {
-    await page.goto('https://pattemdigital.com/');
+    await page.goto('https://pattemdigital.com');
+    await page.waitForLoadState('networkidle');
     await page.getByRole('button', { name: 'Contact us', exact: true }).click();
     await expect(page.locator('#modal__content--contact-us')).toBeVisible();
-    await page.getByPlaceholder('Enter Your Name').click();
-    await page.getByPlaceholder('Enter Your Name').fill('surya');
-    await page.getByPlaceholder('Enter Your Email Address').click();
-    await page.getByPlaceholder('Enter Your Email Address').fill('tanya@playwright.com');
-   await page.getByPlaceholder('Enter Phone Number').click();
-   await page.getByPlaceholder('Enter Phone Number').fill('9876543210');
+    await page.getByPlaceholder('Enter Your Name').nth(0).fill('Surya');
+    await page.getByPlaceholder('Enter Your Email Address').nth(0).fill('tanya@playwright.com');
+    await page.getByPlaceholder('Enter Phone Number').nth(0).fill('9876543210');
   
     await page.getByRole('button', { name: 'Select the options' }).click();
     await page.click('text=Job Seeker');
     const resumePath = path.resolve("C:\\Users\\surya\\Downloads\\Reshma S Babu CV.pdf");
     await page.setInputFiles('input[type="file"]', resumePath);
     await page.locator('#modal__content--contact-us #button--submit-contact-us').click();
-    await page.waitForTimeout(5000); 
-    await expect(page.locator('#form-success__message--contact-us')).toBeVisible({ timeout: 10000 });
+    await page.waitForLoadState('networkidle');
+    await expect(page.locator('#form-success__message--contact-us')).toBeVisible
+    await page.waitForLoadState('networkidle');
     });
 
 // selected the client from dropdown and upload the resume and submit the form  //
 
 test('Fill PattemDigital Contact Form Client', async ({ page }) => {
-    await page.goto('https://pattemdigital.com/');
+    await page.goto('https://www.pattemdigital.com');
     await page.getByRole('button', { name: 'Contact us', exact: true }).click();
-    await expect(page.locator('#modal__content--contact-us')).toBeVisible();
-    await page.getByRole('textbox', { name: 'Enter Your Name' }).click(); 
-    await page.getByRole('textbox', { name: 'Enter Your Name' }).fill('surya');
-    await page.getByRole('textbox', { name: 'Enter Your Email Address' }).click();
-    await page.getByRole('textbox', { name: 'Enter Your Email Address' }).fill('SuRya@playwright.com');
-    await page.getByRole('textbox', { name: 'Enter Phone Number' }).click();
-    await page.getByRole('textbox', { name: 'Enter Phone Number' }).fill('9876543210');
+    await expect(page.locator('#modal__content--contact-us')).toBeVisible(); 
+    await page.waitForLoadState('networkidle')
+    await page.getByPlaceholder('Enter Your Name').nth(1).fill('Surya');
+    await page.getByRole('textbox', { name: 'Enter Your Email Address' }).nth(1).fill('SuRya@playwright.com');
+    await page.getByRole('textbox', { name: 'Enter Phone Number' }).nth(1).fill('9876543210');
   
     await page.getByRole('button', { name: 'Select the options' }).click();
     await page.locator('div').filter({ hasText: /^Client$/ }).click();
@@ -43,27 +40,24 @@ test('Fill PattemDigital Contact Form Client', async ({ page }) => {
     const resumePath = path.resolve("C:\\Users\\surya\\Downloads\\Reshma S Babu CV.pdf");
     await page.locator('#clientDocument').setInputFiles(resumePath);
     await page.locator('#modal__content--contact-us #button--submit-contact-us').click();
-    await page.waitForTimeout(5000); 
-    await expect(page.locator('#form-success__message--contact-us')).toBeVisible({ timeout: 10000 });
+    await page.waitForLoadState('networkidle');
+    await expect(page.locator('#form-success__message--contact-us')).toBeVisible
+    await page.waitForLoadState('networkidle');
   });
 
     // selected the Vendor from dropdown and upload the resume and submit the form  //
 
 
 test('Fill PattemDigital Contact Form vendor', async ({ page }) => {
-    await page.goto('https://pattemdigital.com/');
+    await page.goto('https://pattemdigital.com');
     await page.getByRole('button', { name: 'Contact us', exact: true }).click();
     await expect(page.locator('#modal__content--contact-us')).toBeVisible();
-    await page.getByPlaceholder('Enter Your Name').click();
-    await page.getByPlaceholder('Enter Your Name').fill('surya');
-    await page.getByPlaceholder('Enter Your Email Address').click();
-    await page.getByPlaceholder('Enter Your Email Address').fill('tanya@playwright.com');
-    await page.getByPlaceholder('Enter Phone Number').click();
-    await page.getByPlaceholder('Enter Phone Number').fill('9876543210');
+    await page.getByPlaceholder('Enter Your Name').nth(2).fill('Surya');
+    await page.getByPlaceholder('Enter Your Email Address').nth(2).fill('tanya@playwright.com');
+    await page.getByPlaceholder('Enter Phone Number').nth(2).fill('9876543210');
   
     await page.getByRole('button', { name: 'Select the options' }).click();
     await page.locator('div').filter({ hasText: /^Vendor$/ }).click();
-    await page.getByPlaceholder('Enter Your Organization Name').clear();
     await page.getByPlaceholder('Enter Your Organization Name').fill('Tech Innovations Ltd.');
     await page.getByLabel('Choose your sector').click();
     await page.locator('[data-value="staffing"]').click();
@@ -73,6 +67,7 @@ test('Fill PattemDigital Contact Form vendor', async ({ page }) => {
     const resumePath = path.resolve("C:\\Users\\surya\\Downloads\\Reshma S Babu CV.pdf");
     await page.locator('#vendorDocument').setInputFiles(resumePath);
     await page.locator('#modal__content--contact-us #button--submit-contact-us').click();
-    await page.waitForTimeout(5000); 
-    await expect(page.locator('#form-success__message--contact-us')).toBeVisible({ timeout: 5000 });
+    await page.waitForLoadState('networkidle');
+    await expect(page.locator('#form-success__message--contact-us')).toBeVisible
+    await page.waitForLoadState('networkidle');
 });
