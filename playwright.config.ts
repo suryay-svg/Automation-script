0bff7706c437875f95de7e211377b27642988dca
surya <surya.y@pattemdigital.com>
2025-12-15 15:47:55 +0530

    new feature added in config

--- playwright.config.ts
+++ playwright.config.ts
@@ -29,14 +29,15 @@ export default defineConfig({
     // baseURL: 'http://localhost:3000',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on-first-retry',
+    //trace: 'on-first-retry',
+    //screenshot: 'only-on-failure',
+    //video: 'retain-on-failure',
   },
 
-  /* Configure projects for major browsers */
   projects: [
     {
       name: 'chromium',
-      use: { ...devices['Desktop Chrome'] },
+      use: { ...devices['Desktop Chrome'], viewport: { width: 1280, height: 720 } },
     },
 
     {
